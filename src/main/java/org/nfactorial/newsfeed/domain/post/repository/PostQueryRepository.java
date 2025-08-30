fe5827179569c07da380eb0c3fb1e04cb930c6ab
abko <abko121115@gmail.com>
2025-08-30 13:28:23 +0900

    docs: 사용하지 않는 findViewPostProjectionByPostId 메서드 주석 제거

--- src/main/java/org/nfactorial/newsfeed/domain/post/repository/PostQueryRepository.java
+++ src/main/java/org/nfactorial/newsfeed/domain/post/repository/PostQueryRepository.java
@@ -85,57 +85,6 @@ public class PostQueryRepository {
             .fetch();
     }
 
-    // public Optional<PostViewProjection> findViewPostProjectionByPostId(long postId, long currentUserId) {
-    //     QPost post = QPost.post;
-    //     QProfile postAuthor = new QProfile("postAuthor");
-    //     QUpload upload = QUpload.upload;
-    //     QComment comment = QComment.comment;
-    //     QProfile commentAuthor = new QProfile("commentAuthor");
-    //     QLike like = QLike.like;
-
-    //     Map<Long, PostViewProjection> result = queryFactory
-    //         .from(post)
-    //         .join(post.profile, postAuthor)
-    //         .leftJoin(upload).on(upload.post.id.eq(postId))
-    //         .leftJoin(comment).on(comment.post.id.eq(postId))
-    //         .leftJoin(comment.profile, commentAuthor)
-    //         .where(post.id.eq(postId))
-    //         .groupBy(post.id, postAuthor.id, postAuthor.nickname, post.content,
-    //             post.likeCount, post.viewCount, post.createdAt, post.modifiedAt, upload.id, upload.publicName,
-    //             comment.id, commentAuthor.id, commentAuthor.nickname, comment.content)
-    //         .transform(
-    //             GroupBy.groupBy(post.id).as(
-    //                 Projections.constructor(
-    //                     PostViewProjection.class,
-    //                     post.id,
-    //                     postAuthor.id,
-    //                     postAuthor.nickname,
-    //                     post.content,
-    //                     post.likeCount,
-    //                     comment.id.countDistinct().longValue(),
-    //                     post.viewCount,
-    //                     JPAExpressions.selectOne()
-    //                         .from(like)
-    //                         .where(like.post.id.eq(postId).and(like.profile.id.eq(currentUserId)))
-    //                         .exists(),
-    //                     post.createdAt,
-    //                     post.modifiedAt,
-    //                     GroupBy.list(
-    //                         Projections.constructor(
-    //                             PostViewProjection.SimpleFileProjection.class,
-    //                             upload.id,
-    //                             upload.publicName).skipNulls()),
-    //                     GroupBy.list(
-    //                         Projections.constructor(
-    //                             PostViewProjection.SimpleCommentProjection.class,
-    //                             comment.id,
-    //                             commentAuthor.id,
-    //                             commentAuthor.nickname,
-    //                             comment.content,
-    //                             Expressions.nullExpression(Boolean.class)).skipNulls()))));
-    //     return Optional.ofNullable(result.get(postId));
-    // }
-
     public Set<Long> findParentCommentsInCommentIds(List<Long> commentIds) {
         QComment comment = QComment.comment;
         return new HashSet<Long>(
